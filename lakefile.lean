a33cc6576eac660289f9e769aa4a93495bf497a6
taterowney <51540870+taterowney@users.noreply.github.com>
2026-03-31 11:19:29 -0400

    bump to 4.28

--- lakefile.lean
+++ lakefile.lean
@@ -10,10 +10,10 @@ abbrev safeVerifyLeanOptions : Array LeanOption := #[
 package SafeVerify
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git" @ "v4.27.0"
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.28.0"
 
 require Cli from git
-  "https://github.com/leanprover/lean4-cli.git" @ "v4.27.0"
+  "https://github.com/leanprover/lean4-cli.git" @ "v4.28.0"
 
 @[default_target]
 lean_lib SafeVerify where
